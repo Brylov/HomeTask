0bf9fd683b06b597e94d1a877de05c690b96db42
Guy Braylovski <guybrylov@gmail.com>
2023-04-11 22:03:00 +0300

    first commit

---
+++ app/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Hello Home Task</title>
+</head>
+<body>
+	<h1>Hello Home Task!</h1>
+	<p>This is a sample PHP application that you can use as a starting point for your home task.</p>
+	<p>Your task instructions should give you more information on what you need to build.</p>
+</body>
+</html>
