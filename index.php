8fe4dd549ff4d39795fa70887f91a02811e9149b
Guy Braylovski <guybrylov@gmail.com>
2023-04-12 12:10:31 +0300

    helm charts

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 	<title>Hello Home Task Demo</title>
 </head>
 <body>
-	<h1>Hello Home Task TEST1 !</h1>
+	<h1>Hello Home Task Dev Test !</h1>
 	<p>This is a sample PHP application that you can use as a starting point for your home task.</p>
 	<p>Your task instructions should give you more information on what you need to build.</p>
 </body>
