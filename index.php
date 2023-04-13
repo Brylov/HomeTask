63d6becb1d6cd09009ecd4fd483d6b8becf76ea7
Guy Braylovski <guybrylov@gmail.com>
2023-04-13 13:19:15 +0300

    dev test before interview

--- index.php
+++ index.php
@@ -4,8 +4,9 @@
 	<title>Hello Home Task Demo</title>
 </head>
 <body>
-	<h1>Hello Home Task HelloHello !</h1>
+	<h1>Hello Home Task Dev Test #1 !</h1>
 	<p>This is a sample PHP application that you can use as a starting point for your home task.</p>
 	<p>Your task instructions should give you more information on what you need to build.</p>
 </body>
 </html>
+
